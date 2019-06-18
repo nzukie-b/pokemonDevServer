c901e1caa625df8afe0a5cdccd24cfde84a4e051
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2019-06-18 16:03:12 -0400

    changed to long

--- src/main/java/com/example/s1_project_server/models/User.java
+++ src/main/java/com/example/s1_project_server/models/User.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private long id;
     @NaturalId
     private String username;
     private String password;
@@ -16,11 +16,11 @@ public class User {
     private String lastName;
     private String role;
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
