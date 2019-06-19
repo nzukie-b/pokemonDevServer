ecdc49071bcd54d676823c964b61ebd270e842cc
nzukie <nzukie.b@husky.neu.edu>
2019-06-19 16:07:11 -0400

    User collection only disallows collecting of multiple pokemon

--- src/main/java/com/example/s1_project_server/controllers/UserController.java
+++ src/main/java/com/example/s1_project_server/controllers/UserController.java
@@ -11,79 +11,80 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000", "*"})
+@CrossOrigin(origins = { "http://localhost:3000", "*" })
 public class UserController {
 
-    @Autowired
-    UserRepository ur;
+	@Autowired
+	UserRepository ur;
 
-    @Autowired
-    PokemonRepository pr;
+	@Autowired
+	PokemonRepository pr;
 
+	@GetMapping("/api/users")
+	public List<User> findAllUsers() {
+		return (List<User>) ur.findAll();
+	}
 
-    @GetMapping("/api/users")
-    public List<User> findAllUsers() {
-        return (List<User>) ur.findAll();
-    }
+	@GetMapping("/api/users/{userId}")
+	public User findUserById(@PathVariable("userId") long id) {
+		if (ur.findById(id).isPresent()) {
+			return ur.findById(id).get();
+		} else {
+			return null;
+		}
+	}
 
-    @GetMapping("/api/users/{userId}")
-    public User findUserById(@PathVariable("userId") long id) {
-        if (ur.findById(id).isPresent()) {
-            return ur.findById(id).get();
-        } else {
-            return null;
-        }
-    }
+	@PostMapping("/api/users")
+	public User addUser(@RequestBody User newUser) {
+		return ur.save(newUser);
+	}
 
-    @PostMapping("/api/users")
-    public User addUser(@RequestBody User newUser) {
-        return ur.save(newUser);
-    }
+	@PutMapping("/api/users/{userId}")
+	public User updateUser(@PathVariable long userId, @RequestBody User user) {
+		User u = ur.findById(userId).get();
+		u.setAll(user);
+		ur.save(u);
+		return findUserById(userId);
+	}
 
-    @PutMapping("/api/users/{userId}")
-    public User updateUser(@PathVariable long userId,
-                                 @RequestBody User user) {
-        User u = ur.findById(userId).get();
-        u.setAll(user);
-        ur.save(u);
-        return findUserById(userId);
-    }
+	@DeleteMapping("/api/users/{userId}")
+	public List<User> deleteUser(@PathVariable long userId) {
+		ur.deleteById(userId);
+		return findAllUsers();
+	}
 
-    @DeleteMapping("/api/users/{userId}")
-    public List<User> deleteUser(@PathVariable long userId){
-        ur.deleteById(userId);
-        return findAllUsers();
-    }
+	@GetMapping("/api/users/username/{username}/password/{password}")
+	public User findUserByCredentials(@PathVariable("username") String username,
+			@PathVariable("password") String password) {
+		return ur.findUserByCredentials(username, password);
+	}
 
-    @GetMapping("/api/users/username/{username}/password/{password}")
-    public User findUserByCredentials(
-            @PathVariable("username") String username,
-            @PathVariable("password") String password) {
-        return ur.findUserByCredentials(username, password);
-    }
+	@PutMapping("/api/users/{userId}/pokemon/{pokeId}")
+	public User addPokemonToCollection(@PathVariable(name = "userId") long userId,
+			@PathVariable(name = "pokeId") long pokeId) {
+		User u = ur.findById(userId).get();
+		Pokemon p;
+		if (pr.findById(pokeId).isPresent()) {
+			p = pr.findById(pokeId).get();
+		} else {
+			p = new Pokemon(pokeId);
+			pr.save(p);
+		}
+		ArrayList<Pokemon> newCollection = new ArrayList<Pokemon>(u.getCollectedPokemon());
+		
+		if (!newCollection.contains(p)) {
+			newCollection.add(p);
+			u.setCollectedPokemon(newCollection);
+			System.out.println(newCollection);
+			ur.save(u);
 
-    @PutMapping("/api/users/{userId}/pokemon/{pokeId}")
-    public User addPokemonToCollection(@PathVariable(name="userId") long userId,
-                           @PathVariable(name = "pokeId") long pokeId) {
-        User u = ur.findById(userId).get();
-        Pokemon p;
-        if(pr.findById(pokeId).isPresent()){
-            p = pr.findById(pokeId).get();
-        } else {
-            p = new Pokemon(pokeId);
-            pr.save(p);
-        }
-        ArrayList<Pokemon> newCollection = new ArrayList<Pokemon>(u.getCollectedPokemon());
-        newCollection.add(p);
-        u.setCollectedPokemon(newCollection);
-        System.out.println(newCollection);
-        ur.save(u);
+			ArrayList<User> newCollection2 = new ArrayList<User>(p.getUsers());
+			newCollection2.add(u);
+			p.setUsers(newCollection2);
+			pr.save(p);
+		}
 
-        ArrayList<User> newCollection2 = new ArrayList<User>(p.getUsers());
-        newCollection2.add(u);
-        p.setUsers(newCollection2);
-        pr.save(p);
+		return findUserById(userId);
+	}
 
-        return findUserById(userId);
-    }
 }
