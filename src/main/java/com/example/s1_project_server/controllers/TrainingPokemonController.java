48a8e5decb8ecb3410d511b8e0cf8a7c07108edc
Benjamin Herzberg <herzberg.b@husky.neu.edu>
2019-06-22 19:50:00 -0400

    Added way to get training pokemon from a pokemon name

--- src/main/java/com/example/s1_project_server/controllers/TrainingPokemonController.java
+++ src/main/java/com/example/s1_project_server/controllers/TrainingPokemonController.java
@@ -10,65 +10,75 @@ import com.example.s1_project_server.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = { "http://localhost:3000", "*" })
+@CrossOrigin(origins = {"http://localhost:3000", "*"})
 public class TrainingPokemonController {
 
-	@Autowired
-	UserRepository ur;
+    @Autowired
+    UserRepository ur;
 
-	@Autowired
-	PokemonRepository pr;
+    @Autowired
+    PokemonRepository pr;
 
-	@Autowired
-	TrainingPokemonRepository trainingRepo;
+    @Autowired
+    TrainingPokemonRepository trainingRepo;
 
-	@PostMapping("/api/users/{userId}/team/{pokeId}")
-	public List<TrainingPokemon> addPokemonToTeam(@PathVariable("userId") long userId,
-			@PathVariable("pokeId") long pokeId) {
-		User u = ur.findById(userId).get();
-		if (u.getRole() == UserRole.TRAINER && u.getTeam().size() < 6) {
-			Pokemon p = pr.findById(pokeId).get();
-			TrainingPokemon tp = new TrainingPokemon();
-			tp.setPokeId(p);
-			tp.setUserId(u);
-			trainingRepo.save(tp);
-		}
-		return trainingRepo.findTeamByUserId(userId);
-	}
+    @PostMapping("/api/users/{userId}/team/{pokeId}")
+    public List<TrainingPokemon> addPokemonToTeam(@PathVariable("userId") long userId,
+                                                  @PathVariable("pokeId") long pokeId) {
+        User u = ur.findById(userId).get();
+        if (u.getRole() == UserRole.TRAINER && u.getTeam().size() < 6) {
+            Pokemon p = pr.findById(pokeId).get();
+            TrainingPokemon tp = new TrainingPokemon();
+            tp.setPokeId(p);
+            tp.setUserId(u);
+            trainingRepo.save(tp);
+        }
+        return trainingRepo.findTeamByUserId(userId);
+    }
 
-	@DeleteMapping("/api/users/{userId}/team/{trainingPokeId}")
-	public List<TrainingPokemon> deletePokemonFromTeam(@PathVariable("userId") long userId,
-			@PathVariable("trainingPokeId") long trainingPokeId) {
-		trainingRepo.deleteById(trainingPokeId);
-		return trainingRepo.findTeamByUserId(userId);
-	}
+    @DeleteMapping("/api/users/{userId}/team/{trainingPokeId}")
+    public List<TrainingPokemon> deletePokemonFromTeam(@PathVariable("userId") long userId,
+                                                       @PathVariable("trainingPokeId") long trainingPokeId) {
+        trainingRepo.deleteById(trainingPokeId);
+        return trainingRepo.findTeamByUserId(userId);
+    }
 
-	@GetMapping("/api/users/{userId}/team")
-	public List<TrainingPokemon> findAllTrainingPokemon(@PathVariable("userId") long userId) {
-		return trainingRepo.findTeamByUserId(userId);
-	}
+    @GetMapping("/api/users/{userId}/team")
+    public List<TrainingPokemon> findAllTrainingPokemon(@PathVariable("userId") long userId) {
+        return trainingRepo.findTeamByUserId(userId);
+    }
 
-	@GetMapping("/api/users/{userId}/team/{trainingPokeId}")
-	public TrainingPokemon findTrainingPokemonByUserAndPokemon(@PathVariable("userId") long userId,
-			@PathVariable("trainingPokeId") long trainingPokeId) {
-		if (trainingRepo.existsById(trainingPokeId)) {
-			return trainingRepo.findById(trainingPokeId).get();
-		}
-		return null;
-	}
+    @GetMapping("/api/users/{userId}/team/{trainingPokeId}")
+    public TrainingPokemon findTrainingPokemonByUserAndPokemon(@PathVariable("userId") long userId,
+                                                               @PathVariable("trainingPokeId") long trainingPokeId) {
+        if (trainingRepo.existsById(trainingPokeId)) {
+            return trainingRepo.findById(trainingPokeId).get();
+        }
+        return null;
+    }
 
-	@PutMapping("/api/users/{userId}/team/{trainingPokeId}")
-	public List<TrainingPokemon> updatePokemonOnTeam(@PathVariable("userId") long userId,
-			@PathVariable("trainingPokeId") long trainingPokeId, @RequestBody TrainingPokemon updatedPokemon) {
-		User u = ur.findById(userId).get();
-		if (u.getRole() == UserRole.TRAINER && u.getTeam().size() <= 6) {
-			TrainingPokemon tp = trainingRepo.findById(trainingPokeId).get();
-			tp.setLevel(updatedPokemon.getLevel());
-			trainingRepo.save(tp);
-		}
-		return trainingRepo.findTeamByUserId(userId);
-	}
+    @PutMapping("/api/users/{userId}/team/{trainingPokeId}")
+    public List<TrainingPokemon> updatePokemonOnTeam(@PathVariable("userId") long userId,
+                                                     @PathVariable("trainingPokeId") long trainingPokeId, @RequestBody TrainingPokemon updatedPokemon) {
+        User u = ur.findById(userId).get();
+        if (u.getRole() == UserRole.TRAINER && u.getTeam().size() <= 6) {
+            TrainingPokemon tp = trainingRepo.findById(trainingPokeId).get();
+            tp.setLevel(updatedPokemon.getLevel());
+            trainingRepo.save(tp);
+        }
+        return trainingRepo.findTeamByUserId(userId);
+    }
+
+    @GetMapping("/api/users/pokemon/{pokemonId}")
+    public List<TrainingPokemon> findPokemonInCollectionIrellevantOfUsers(@PathVariable(name = "pokemonId") String pokeId) {
+        if (!(pr.findPokemonByName(pokeId).size() > 0)) {
+			return new ArrayList<>();
+        } else {
+           return pr.findPokemonByName(pokeId).get(0).getChildren();
+        }
+    }
 }
--- src/main/java/com/example/s1_project_server/repositories/PokemonRepository.java
+++ src/main/java/com/example/s1_project_server/repositories/PokemonRepository.java
@@ -1,7 +1,16 @@
 package com.example.s1_project_server.repositories;
 
 import com.example.s1_project_server.models.Pokemon;
+import com.example.s1_project_server.models.TrainingPokemon;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PokemonRepository extends CrudRepository<Pokemon, Long> {
+
+    @Query("SELECT pokemon from Pokemon pokemon WHERE pokemon.name=:name")
+    public List<Pokemon> findPokemonByName(
+            @Param("name") String name);
 }
